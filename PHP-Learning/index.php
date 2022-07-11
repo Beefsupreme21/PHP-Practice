7745602765108075c625064e5c711ab067080fce
Cory S <101227906+Beefsupreme21@users.noreply.github.com>
2022-07-11 17:30:43 -0600

    Add files via upload

---
+++ PHP-Learning/index.php
@@ -0,0 +1,18 @@
+ <!DOCTYPE html>
+<html>
+<body>
+
+<h1>
+<?php
+
+$greeting = 'Hello World245';
+$name = $_GET['name'];
+
+echo 'Hello, ' . $greeting . ' ' . $name
+?>
+</h1>
+
+
+</body>
+</html> 
+
