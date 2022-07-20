7c390f4010744817c8de47c3b1fdf5ce6991d622
Cory S <101227906+Beefsupreme21@users.noreply.github.com>
2022-07-20 16:46:16 -0600

    Add files via upload

---
+++ PHP-Learning/PHP Forms/views/index.view.php
@@ -0,0 +1,12 @@
+<?php require('partials/head.php'); ?>
+
+<h1>Submit Your Name</h1>
+
+<form method="GET" action="/names">
+    <input name="name">
+</form>
+<button type="submit">Submit</button>
+
+<?php require('partials/footer.php'); ?>
+
+ 
